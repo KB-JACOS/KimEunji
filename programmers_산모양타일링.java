e944415585c35ba2fe9e79a4ee41c3e2ba8a175b
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-10-09 02:56:30 +0900

    solved: programmers_산모양타일링

--- programmers_산모양타일링.java
+++ programmers_산모양타일링.java
@@ -1,46 +1,19 @@
-
-import java.util.*;
-public class programmers_산모양타일링 {
-    static int count = -1;
-    static List<int[]> tri;
-
-    public static void main(String[] args) {
-        System.out.println(solution(2, new int[]{0,1}));
-    }
-
-    public static int solution(int n, int[] tops) {
+//[참고] https://tolerblanc.github.io/programmers/programmers-mountain-tiling/
+class Solution {
+    public int solution(int n, int[] tops) {
         int answer = 0;
-        tri = new ArrayList<>();
-        for(int i=1; i<=2*n; i++){
-            tri.add(new int[]{i, i+1});
+        int[] dp1 = new int[n+1];
+        int[] dp2 = new int[n+1];
+        
+        dp1[1] = 1; 
+        dp2[1] = tops[0] + 2;
+        
+        for(int i=2; i<=n; i++){
+            // %10007 : 계산 중간 오버플로 방지
+            dp1[i] = (dp1[i-1] + dp2[i-1]) % 10007;
+            dp2[i] = (dp1[i-1]*(tops[i-1]+1) + dp2[i-1]*(tops[i-1]+2)) % 10007;
         }
-
-        for(int i=0; i<tops.length; i++){
-            if(tops[i] == 1){
-                tri.add(new int[]{2*(i+1), 2*n + (i+1)});
-            }
-        }
-
-        // for(int[] t: tri){
-        //     System.out.println(Arrays.toString(t));
-        // }
-        //comb(0, 0, new ArrayList<>());
-        return count;
+        
+        return (int)((dp1[n]+dp2[n])%10007);
     }
-
-//    private static void comb(int start, int depth, List<Integer> tmp){
-//        count ++;
-//        System.out.println(tmp);
-//        if(depth == tri.size()) return;
-//
-//        for(int i=start; i<tri.size(); i++){
-//            for(Integer t : tmp) {
-//                if(tri.get(t) == tri.get(i)[0] || tri.get(t).equals(tri.get(i)[1])) continue;
-//            }
-//
-//            tmp.add(i);
-//            comb(i+1, depth+1, tmp);
-//            tmp.remove(tmp.size()-1);
-//        }
-//    }
 }
